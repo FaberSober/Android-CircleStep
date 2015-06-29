f3218f877d1cb5e91a1fef13a9f81b0f2974f809
FaberSober <1508075252@qq.com>
2015-06-29 17:30:01 +0800

    V1.0 Commit

--- demo/src/main/java/com/faber/circlestepview/demo/MainActivity.java
+++ demo/src/main/java/com/faber/circlestepview/demo/MainActivity.java
@@ -1,38 +1,27 @@
 package com.faber.circlestepview.demo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.faber.circlestepview.CircleStepView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private CircleStepView mCircleStepView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mCircleStepView = ((CircleStepView) findViewById(R.id.circle_step_view));
+        mCircleStepView.setTextBelowCircle("Step 1", "Step 2", "Step 3");
+        mCircleStepView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCircleStepView.setCurrentCircleIndex(mCircleStepView.getCurrentCircleIndex() + 1, true);
+            }
+        });
     }
 }
